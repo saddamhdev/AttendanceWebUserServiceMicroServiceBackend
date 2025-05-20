36617fa029027cfa1f4196e31cae849640302dd7
Saddam <01957987631a@gmail.com>
2025-05-20 15:58:37 +0600

    code update

--- src/test/java/com/example/userservice/MongoDBConnectionTest.java
+++ src/test/java/com/example/userservice/MongoDBConnectionTest.java
@@ -9,10 +9,6 @@ public class MongoDBConnectionTest {
         // String uri = "mongodb://admin:ce18046@13.51.206.164:27017/admin?authSource=admin"; // Your MongoDB URI
         String uri = "mongodb://snvn:H3Af5kWz5PhhS8H8@159.89.172.251:27017/?authSource=snvn&tls=false"; // Your MongoDB URI
 
-<<<<<<< HEAD
-
-=======
->>>>>>> e578cacc7894235eed0f181de9721cbe75df770f
         try (MongoClient mongoClient = MongoClients.create(uri)) {
             MongoDatabase database = mongoClient.getDatabase("admin");
             Document ping = database.runCommand(new Document("ping", 1));
